1b86b2147f69abf99f0f9325a8bebc44d1b020d1
John Barton <john.developer.mail@gmal.com>
2021-09-15 15:55:10 -0700

    Subtraction added

--- src/main/java/MathHelper.java
+++ src/main/java/MathHelper.java
@@ -3,4 +3,8 @@ public class MathHelper {
     public static int add(int a, int b){
         return a + b;
     }
+
+    public static int sub(int a, int b){
+        return a - b;
+    }
 }
--- src/test/java/MathHelperTest.java
+++ src/test/java/MathHelperTest.java
@@ -2,5 +2,6 @@ public class MathHelperTest {
 
     public static void main(String[] args){
         System.out.println(MathHelper.add(10, 20));
+        System.out.println(MathHelper.sub(100, 40));
     }
 }
