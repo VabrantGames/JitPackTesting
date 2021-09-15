4abffd2714e31856313df008a84f1fe647c822d0
John Barton <john.developer.mail@gmal.com>
2021-09-14 23:33:35 -0700

    Addition added

---
+++ src/main/java/MathHelper.java
@@ -0,0 +1,6 @@
+public class MathHelper {
+
+    public static int add(int a, int b){
+        return a + b;
+    }
+}
---
+++ src/test/java/MathHelperTest.java
@@ -0,0 +1,6 @@
+public class MathHelperTest {
+
+    public static void main(String[] args){
+        System.out.println(MathHelper.add(10, 20));
+    }
+}
