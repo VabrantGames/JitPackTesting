8f7e70138634daf510e74addac951729af9b92de
John Barton <john.developer.mail@gmal.com>
2021-10-13 21:29:38 -0700

    bob

--- math/src/test/java/MathHelperTest.java
+++ math/src/test/java/MathHelperTest.java
@@ -1,7 +1,15 @@
+import space.earlygrey.shapedrawer.ShapeDrawer;
+
+import static org.junit.Assert.assertTrue;
+
 public class MathHelperTest {
+
+    ShapeDrawer shapeDrawer;
 
     public static void main(String[] args){
         System.out.println(MathHelper.add(10, 20));
         System.out.println(MathHelper.sub(100, 40));
+
+        assertTrue(true);
     }
 }
